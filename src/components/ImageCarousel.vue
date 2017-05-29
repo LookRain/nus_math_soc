0a6e1950d382a102f3b1e31de5deca9cb22d1288
LookRain <brantscube@hotmail.com>
2017-05-30 02:50:53 +0800

    exact out carousel component

--- src/components/ImageCarousel.vue
+++ src/components/ImageCarousel.vue
@@ -1,18 +1,53 @@
 <template>
-<swiper :options="swiperOption">
-                  <swiper-slide><b>test</b>Slide 1</swiper-slide>
-                  <swiper-slide v-for="imgs in carouselImages"><my-component></my-component></swiper-slide>
-                  <div class="swiper-pagination" slot="pagination"></div>
-                </swiper>
-</template>
-<script>
-export default {
-  name: 'ImageCarousel',
-  data () {
-    return {
+	<div class="card">
+
+		<div class="card-content has-text-centered" id="carousel">
+			<!-- swiper -->
+			<swiper :options="swiperOption">
+				<swiper-slide v-for="img in carouselImages"><img :src="getImageUrl(img.link)" style="width:100%;">
+					<p><b>{{ img.des }}</b></p></swiper-slide>
+					<div class="swiper-pagination" slot="pagination"></div>
+				</swiper>
+
+			</div>
+
+		</div>
+	</template>
+	<script>
+		export default {
+		  name: 'ImageCarousel',
+		  data () {
+		    return {
+		      swiperOption: {
+		        pagination: '.swiper-pagination',
+		        paginationClickable: true,
+		        autoplay: 3000,
+		        autoplayDisableOnInteraction: false，
+		        autoHeight: true
+		      },
+		      link: 'AGM_2016.jpg',
+		      carouselImages: [
+					{link: 'AGM_2016', des: 'Maths Society Annual General Meeting 2016'},
+					{link: 'cubeSchool', des: 'Singapore Rubik’s Cube Competitions 2015 School Category'},
+					{link: 'latex', des: 'LaTeX Workshop 2015'},
+					{link: 'sudoku', des: 'NUS Sudoku Competition 2014'}
+		      ]
+		    }
+		  },
+		  methods: {
+		    getImageUrl (imgName) {
+		      var images = require.context('../assets/images/photos/', false, /\.jpg$/)
+      		// var imgArray = this.carouselImages2
+      		return images('./' + imgName + '.jpg')
     }
   }
 }
 </script>
 <style lang="css" scoped>
+	#carousel {
+		padding: 0rem;
+	}
+	.swiper-pagination {
+		padding-bottom: 50px;
+	}
 </style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <section class="hero is-primary is-bold">
+    <section class="hero is-info is-bold">
       <div class="hero-body">
         <div class="container">
           <div class="columns">
@@ -22,20 +22,7 @@
 
         <div class="columns">
           <div class="column is-8">
-            <div class="card">
-             
-              <div class="card-content" id="carousel">
-                <!-- swiper -->
-                <swiper :options="swiperOption">
-                  <!-- <swiper-slide><b>test</b>Slide 1</swiper-slide>
-                  <swiper-slide>{{ carouselImages[1] }}</swiper-slide> -->
-                  <!-- <swiper-slide v-for="img in carouselImages"><img :src="getUrl(1)" :alt="img.link" :key="img.link"></swiper-slide> -->
-                  <swiper-slide v-for="img in carouselImages2"><img :src="getUrl(img)"></swiper-slide>
-                  <div class="swiper-pagination" slot="pagination"></div>
-                </swiper>
-              </div>
-
-            </div>
+            <image-carousel></image-carousel>
           </div>
 
           <div class="column is-4">
@@ -68,50 +55,25 @@
 </template>
 
 <script>
+import ImageCarousel from '@/components/ImageCarousel'
 
 export default {
   name: 'Home',
-
+  components: { 'image-carousel': ImageCarousel },
   data () {
     return {
       title: '',
       description: ['NUS Mathematics Society is a group of energetic and fun-loving people who organize exciting events for all those who have an interest in Mathematics.',
-        ' NUS Mathematics Society has organized various events ranging from intra-faculty level to international level. Some of the major events include the Singapore Rubik’s Cube Competition, NUS Sudoku Competition, Enrichment Talks and Student Staff Games.'],
-      swiperOption: {
-        pagination: '.swiper-pagination',
-        paginationClickable: true
-      },
-      link: 'AGM_2016.jpg',
-      carouselImages: [
-        {link: 'static/images/photos/AGM_2016.jpg', des: 'img1'},
-        {link: 'static/images/photos/cubeSchool.jpg', des: 'img1'},
-        {link: 'static/images/photos/latex.jpg', des: 'img1'},
-        {link: 'static/images/photos/outing.jpg', des: 'img1'},
-        {link: 'static/images/photos/sudoku.jpg', des: 'img1'}
-      ],
-      carouselImages2: [
-        'AGM_2016',
-        'cubeSchool',
-        'latex',
-        'outing',
-        'sudoku'
-      ]
+        ' NUS Mathematics Society has organized various events ranging from intra-faculty level to international level. Some of the major events include the Singapore Rubik’s Cube Competition, NUS Sudoku Competition, Enrichment Talks and Student Staff Games.']
     }
   },
   methods: {
-    getUrl (imgName) {
-      var images = require.context('../assets/images/photos/', false, /\.jpg$/)
-      // var imgArray = this.carouselImages2
 
-      return images('./' + imgName + '.jpg')
-    }
   }
 
 }
 </script>
 
 <style lang="css" scoped>
-  #carousel {
-    padding: 0rem;
-  }
+
 </style>
