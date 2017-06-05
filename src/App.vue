d2fe8bd5952fdb9661bb92d4e29e3a4811e2b088
LookRain <brantscube@hotmail.com>
2017-06-05 18:26:21 +0800

    add footer

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,37 @@
 <template>
   <div>
-  <my-nav id="nav"></my-nav>
-  <transition name="slide-fade">
-    <router-view></router-view>
+    <my-nav></my-nav>
+    <transition name="slide-fade">
+      <router-view></router-view>
     </transition>
+    <my-footer></my-footer>
   </div>
   
 </template>
 
 <script>
-import MyNav from '@/components/MyNav'
+  import MyNav from '@/components/MyNav'
+  import MyFooter from '@/components/MyFooter'
 
-export default {
-  name: 'app',
-  components: { MyNav }
-}
+  export default {
+    name: 'app',
+    components: { MyNav, MyFooter }
+  }
 </script>
 
 <style lang="css">
-/* Enter and leave animations can use different */
-/* durations and timing functions.              */
-.slide-fade-enter-active {
-  transition: all .2s ease;
-}
-.slide-fade-leave-active {
-  transition: all .4s cubic-bezier(1.0, 0.5, 0.8, 1.0);
-}
-.slide-fade-enter, .slide-fade-leave-to
-/* .slide-fade-leave-active for <2.1.8 */ {
-  transform: translateX(20px);
-  opacity: 0;
-}
+  /* Enter and leave animations can use different */
+  /* durations and timing functions.              */
+  .slide-fade-enter-active {
+    transition: all .2s ease;
+  }
+  .slide-fade-leave-active {
+    transition: all .4s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+  }
+  .slide-fade-enter, .slide-fade-leave-to
+  /* .slide-fade-leave-active for <2.1.8 */ {
+    transform: translateX(20px);
+    opacity: 0;
+  }
 </style>
 
---
+++ src/components/MyFooter.vue
@@ -0,0 +1,21 @@
+<template>
+	<footer class="footer">
+		<div class="container">
+			<div class="content has-text-centered">
+				<p class="text-muted text-right">Copyright © <a href='#'>NUS Mathematics Society</a></p>
+			</div>
+		</div>
+	</footer>
+
+</template>
+<script>
+	export default {
+	  name: 'MyFooter',
+	  data () {
+	    return {
+	    }
+	  }
+	}
+</script>
+<style lang="css" scoped>
+</style>
